31acd544592cb4e9e923e64e1f48a1dbe0742722
initpidzero <me@anuz.me>
2019-04-23 08:57:45 +0100

    4 spaces to 8 spaces of Indent.

--- src/dbg.h
+++ src/dbg.h
@@ -12,35 +12,35 @@ int tester_fn(int *exit, int argc, char **argv);
 
 /* this structure keeps track of breakpoint related data */
 struct bp {
-    uintptr_t addr; /* address where break point is set */
-    unsigned long word; /* content at breakpoint address */
-    unsigned long trap; /* replacement for trap instruction on address */
-    int set; /* 0 = bp is not set. 1 = bp is set.
-              * 2 = breakpoint was hit and needs to be set again */
-    int num; /* nth breakpoint in the series */
+        uintptr_t addr; /* address where break point is set */
+        unsigned long word; /* content at breakpoint address */
+        unsigned long trap; /* replacement for trap instruction on address */
+        int set; /* 0 = bp is not set. 1 = bp is set.
+                  * 2 = breakpoint was hit and needs to be set again */
+        int num; /* nth breakpoint in the series */
 };
 
 /* hardware breakpoint */
 struct hw_bp {
-    uintptr_t addr;/* addr at which hardware breakpoint is set */
-    int set; /* 0 hw bp is not set. 1 = hw bp is set */
-    int num; /* nth hw breakpoint, maximum 4 */
+        uintptr_t addr;/* addr at which hardware breakpoint is set */
+        int set; /* 0 hw bp is not set. 1 = hw bp is set */
+        int num; /* nth hw breakpoint, maximum 4 */
 };
 
 /* Watchpoint */
 struct wp {
-    uintptr_t addr;/* addr at which watchpoint is set */
-    long value;/* value for watchpoint variable. */
-    int set; /* 0 = wp is not set. 1 = wp is set */
-    int num; /* nth watchpoint, maximum 4 */
-    int no_value; /* so this variable is set if there is no value to be watched for */
+        uintptr_t addr;/* addr at which watchpoint is set */
+        long value;/* value for watchpoint variable. */
+        int set; /* 0 = wp is not set. 1 = wp is set */
+        int num; /* nth watchpoint, maximum 4 */
+        int no_value; /* so this variable is set if there is no value to be watched for */
 };
 
 /* signals data structure */
 struct sig_dis {
-    int sig; /* signal recieved */
-    int set; /* whether of not we have a pending signal */
-    int act; /* 0 = signal is ignore. 1 = signal is passed to debuggee. */
+        int sig; /* signal recieved */
+        int set; /* whether of not we have a pending signal */
+        int act; /* 0 = signal is ignore. 1 = signal is passed to debuggee. */
 };
 
 #endif
